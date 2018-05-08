de577b795280ae6599c0890851756ea045d9b995
Nguyen Duc Tam <ductam@CV080011D-PC.cubevn.local>
2018-05-08 15:59:09 +0700

    Master_page update

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -1,7 +1,16 @@
 <?php
-class Pages extends CI_Controller {
-    public function view($page = 'home')
+class Homepage extends My_Controller {
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function index()
     {
-        
+        // Load the subview
+        $content = $this->load->view('homepage/homepage',null, true);
+
+        // Pass to the master view
+        $this->load->view('master_page', array('content' => $content));
     }
 }
--- application/core/My_Controller.php
+++ application/core/My_Controller.php
@@ -3,6 +3,7 @@ class My_Controller extends CI_Controller {
     function __construct(){
         parent::__construct();
         $this->load->library('migration');
+        $this->load->helper('url');
         if ($this->migration->current() === FALSE)
         {
             show_error($this->migration->error_string());
